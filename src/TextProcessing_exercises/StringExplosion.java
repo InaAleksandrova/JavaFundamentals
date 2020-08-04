9aefb82f0f7b98aac954ddc299d9a3306bd8cb02
InaAleksandrova <48957416+InaAleksandrova@users.noreply.github.com>
2020-08-04 22:11:03 +0300

    new commit

--- src/TextProcessing_exercises/StringExplosion.java
+++ src/TextProcessing_exercises/StringExplosion.java
@@ -6,13 +6,14 @@ public class StringExplosion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
         StringBuilder sb = new StringBuilder();
         int power = 0;
 
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
             if (currentChar == '>') {
-                power = Integer.parseInt(String.valueOf(input.charAt(i + 1)));
+                power += Integer.parseInt(String.valueOf(input.charAt(i + 1)));
                 sb.append('>');
             } else if (power == 0) {
                 sb.append(currentChar);
